631c642af6e5beccc56cb81b8b731e5d109662d5
evilbaschdi <sebwalter@gmx.net>
2016-06-17 19:51:47 +0200

    serialization, changes to core logic

--- Movie.Core/MovieRecord.cs
+++ Movie.Core/MovieRecord.cs
@@ -1,22 +1,32 @@
+using System.Runtime.Serialization;
+
 namespace Movie.Core
 {
     /// <summary>
     ///     Description for MovieRecord.
     /// </summary>
+    [DataContract]
     public class MovieRecord : IMovieRecord
     {
+        [DataMember]
         public string Id { get; set; }
 
+        [DataMember]
         public string Name { get; set; }
 
+        [DataMember]
         public string Year { get; set; }
 
+        [DataMember]
         public string Format { get; set; }
 
+        [DataMember]
         public string Distributed { get; set; }
 
+        [DataMember]
         public string DistributedTo { get; set; }
 
+        [DataMember]
         public string Watched { get; set; }
     }
 }
